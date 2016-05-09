4086941486afa711a0cd296ca50e7ce8e843e53e
cbosselut <bossone0013@gmail.com>
2016-05-09 10:41:57 +0200

    use /dev/ttyMCC for Neo

--- jrun.sh
+++ jrun.sh
@@ -3,8 +3,8 @@
 [[ "$(pidof -x $(basename $0))" != $$ ]] && echo "Program already running. Exit." && exit
 
 if [ ! -h /dev/ttyS0 ]; then
-	sudo ln -s /dev/ttymxc3 /dev/ttyS0
-	#sudo ln -s /dev/ttyMCC /dev/ttyS0
+	#sudo ln -s /dev/ttymxc3 /dev/ttyS0
+	sudo ln -s /dev/ttyMCC /dev/ttyS0
 fi
 if [ ! -f /sys/class/i2c-dev/i2c-1/device/1-0048/temp1_input ]; then
 	sudo sh -c 'echo lm75 0x48 >/sys/class/i2c-dev/i2c-1/device/new_device'
